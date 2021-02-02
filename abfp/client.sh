b421de270b680d2fcb8cb81801f68355d7650228
pedrofenti <pedro.fernandez@enti.cat>
2021-02-02 19:37:04 +0100

    Avanzando en el protocolo: hasta el nombre del archivo

--- abfp/client.sh
+++ abfp/client.sh
@@ -7,13 +7,17 @@ IP_CLIENT="127.0.0.1"
 IP_SERVER="127.0.0.1"
 OK="OK_CONN"
 KO="KO_CONN"
+YES="YES_IT_IS"
+OK_FILE="OK_FILE_NAME"
 
 echo "(2) Seanding headers"
 
 echo "ABFP $IP_CLIENT" | nc -q 1 $IP_SERVER $PORT
 
 echo "(3) Listening"
-RESPONSE=`nc -l -p` $PORT
+
+RESPONSE=`nc -l -p $PORT`
+
 if [ "$RESPONSE" != $OK ]; then
 echo "Error: Connection refused"
 exit 1
@@ -24,10 +28,30 @@ echo "(6) Handshake"
 sleep 1
 echo "This_is_my_classroom" | nc -q 1 $IP_SERVER $PORT
 
-
 echo "(7) Listening"
-RESPONSE=`nc -l -p` $PORT
 
+RESPONSE=`nc -l -p $PORT`
+if [ "$RESPONSE" != $YES ]; then
+echo "Error: Connection refused"
+exit 2
+fi
+
+echo "(10) File name"
+
+sleep 1
+echo "File_name Magno.jpg" | nc -q 1 $IP_SERVER $PORT
+
+echo "(11) Listening"
+
+RESPONSE=`nc -l -p $PORT`
+if [ "$RESPONSE" != $OK_FILE ]; then
+echo "Error: Lost connection"
+exit 3
+fi
+
+echo "(14) Data"
+
+sleep 1 
 
 
 exit 0
--- abfp/server.sh
+++ abfp/server.sh
@@ -2,6 +2,8 @@
 
 PORT=2021
 PROT_NAME="ABFP"
+HANDSHAKE="This_is_my_classroom"
+
 
 echo "(0) Alejandro's Big Fucking Protocol"
 
@@ -25,12 +27,36 @@ fi
 sleep 1
 echo "OK_CONN" | nc -q 1 $IP_CLIENT $PORT
 
-echo "(5) Listen"
+echo "(5) Listening"
 
-HANDSHAKE=`nc -l -p 
+CLIENT_HANDSHAKE=`nc -l -p $PORT`
 
+echo "(8) Response"
 
+if [ $CLIENT_HANDSHAKE != $HANDSHAKE ]; then
+echo "Error: Wrong handshake"
+sleep 1
+echo "KO_HANDSHAKE" | nc -q 1 $IP_CLIENT $PORT
+exit 2 
+fi
 
+sleep 1
+echo "YES_IT_IS" | nc -q 1 $IP_CLIENT $PORT
+
+echo "(9) Listening"
+
+FILE_NAME=`nc -l -p $PORT`
+
+echo "(12) Response"
+
+sleep 1
+echo "OK_FILE_NAME" | nc -q 1 $IP_CLIENT $PORT
+
+echo "(13) Listening"
+
+CLIENT_DATA=`nc -l -p $PORT`
+
+echo "(16) Response"
 
 
 exit 0
